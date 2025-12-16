3c8918c091ec3b4c1fc4d127c16e776bfc9be716
Haditech2 <your-email@example.com>
2025-12-16 19:35:31 +0100

    Fix database config for Vercel build

--- ejeh_palace/settings.py
+++ ejeh_palace/settings.py
@@ -76,10 +76,11 @@ WSGI_APPLICATION = 'ejeh_palace.wsgi.application'
 
 # Database
 # PostgreSQL configuration for production
-if os.environ.get('DATABASE_URL'):
+DATABASE_URL = os.environ.get('DATABASE_URL')
+if DATABASE_URL:
     DATABASES = {
         'default': dj_database_url.config(
-            default=os.environ.get('DATABASE_URL'),
+            default=DATABASE_URL,
             conn_max_age=600,
             conn_health_checks=True,
         )
