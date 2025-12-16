1f423c67aae52274d4cc264ffe3dcc8a4b590387
Haditech2 <your-email@example.com>
2025-12-16 19:45:05 +0100

    Fix: Only run migrations if DATABASE_URL is set

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 pip install -r requirements.txt
 python manage.py collectstatic --noinput --ignore admin
-python manage.py migrate --noinput
+
+# Only run migrations if DATABASE_URL is set
+if [ -n "$DATABASE_URL" ]; then
+    python manage.py migrate --noinput
+fi
